1eb4269baf25b4f02e1cebb256c075915b43cbb8
Shreyas Pansare <shreyaspansare123@gmail.com>
2021-09-05 12:28:39 +0530

    Employee Management Dashboard
    im very honoured to work with reckadon sir he promised to give me 11 lakh rupees for  my contribution like oh my god so humble person he is. (bhai 11 lakh paytm karde xD)

---
+++ src/components/Charts/Barchart.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Bar } from "react-chartjs-2";
+import "../styles/dashboard.css";
+
+const BarChart = () => {
+  return (
+    <div>
+      <Bar
+        data={{
+          labels: ["2017", "2018", "2019", "2020", "2021"],
+          datasets: [
+            {
+              label: "Total Revenue In Respective Year",
+              data: [7679970, 9702300, 10574629, 9623810, 15173722],
+              backgroundColor: [
+                "rgb(35, 100, 170)",
+                "rgb(54, 162, 235)",
+                "rgb(4, 53, 101)",
+                "rgb(12, 98, 145)",
+                "rgb(45, 147, 173)",
+              ],
+            },
+          ],
+        }}
+        height={300}
+        width={700}
+        style={{
+          marginBottom: "20px",
+          textAlign: "center",
+        }}
+        options={{
+          maintainAspectRatio: false,
+        }}
+      />
+    </div>
+  );
+};
+
+export default BarChart;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,44 @@
 import React, { Component } from "react";
+import "../styles/dashboard.css";
+import DoughnutChart from "../Charts/Doughnut";
+import BarChart from "../Charts/Barchart";
 
 class Dashboard extends Component {
   state = {};
   render() {
-    return <h1 style={{ margin: "0px" }}>Dashboard</h1>;
+    return (
+      <>
+        <div className="mainContainer">
+          <section className="subContainer1">
+            <div className="keyCont">
+              <h4 className="keyContTitle">Total Requests</h4>
+              <h2 className="keyContValue">5680</h2>
+            </div>
+            <div className="keyCont">
+              <h4 className="keyContTitle">Pending Requests</h4>
+              <h2 className="keyContValue">17</h2>
+            </div>
+            <div className="keyCont">
+              <h4 className="keyContTitle">Gender Ratio</h4>
+              <h2 className="keyContValue">16:10</h2>
+            </div>
+            <div className="keyCont">
+              <h4 className="keyContTitle">Total Users</h4>
+              <h2 className="keyContValue">25647</h2>
+            </div>
+          </section>
+          <div className="subContainerWrapper">
+            <section className="subContainer2">
+              <BarChart/>
+            </section>
+            <section className="subContainer3">
+              <h1 className="ageGraphLabel">Age Graph</h1>
+              <DoughnutChart />
+            </section>
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
